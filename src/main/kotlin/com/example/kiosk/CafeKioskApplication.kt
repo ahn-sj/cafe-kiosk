bc79913aac0e6b21958d72fe5a05449763eba7d0
ahn-sj <jaesa5221@gmail.com>
2024-12-26 21:35:40 +0900

    init: first commit

---
+++ src/main/kotlin/com/example/kiosk/CafeKioskApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kiosk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CafeKioskApplication
+
+fun main(args: Array<String>) {
+	runApplication<CafeKioskApplication>(*args)
+}
